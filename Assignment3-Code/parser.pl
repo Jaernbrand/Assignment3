36f44bba4338dfd4fd09539669a60e1345e5ad84
Jaernbrand <henrikjarnbrand@gmail.com>
2015-01-05 17:01:56 +0100

    Hjälppredikat i parsern fungerar i teorin nu.

--- Assignment3-Code/parser.pl
+++ Assignment3-Code/parser.pl
@@ -7,8 +7,10 @@ assign(Tree, Lexemes, VariablesOut) :-
 	expr(Tree, Lexemes, VariablesOut), 
 	assignEnd(Tree, Lexemes).
 
-assignToken(Tree, [L | Lexemes]) :- L '='.
-assignEnd(Tree, [L | Lexemes]) :- L ';'.
+assignToken(Tree, [L | Lexemes]) :-
+	atom_codes(L, [C | Codes]), C == 61. % 61 = '=' 
+assignEnd(Tree, [L | Lexemes]) :- L
+	atom_codes(L, [C | Codes]), C == 59. % 59 = ';' 
 
 expr(Tree, Lexemes, VariablesOut) :- term(Tree, Lexemes)
 	term(Tree, Lexemes).
@@ -18,8 +20,10 @@ expr(Tree, Lexemes, VariablesOut) :- term(Tree, Lexemes)
 	exprOperator(Tree, Lexemes),
 	expr(Tree, Lexemes).
 
-exprOperator(Tree, [L | Lexemes]) :- L '+'.
-exprOperator(Tree, [L | Lexemes]) :- L '-'.
+exprOperator(Tree, [L | Lexemes]) :-
+	atom_codes(L, [C | Codes]), C == 43. % 43 = '+' 
+exprOperator(Tree, [L | Lexemes]) :-
+	atom_codes(L, [C | Codes]), C == 45. % 45 = '-' 
 
 term(Tree, Lexemes) :- factor(Tree, Lexemes) :-
 	factor(Tree, Lexemes).
@@ -29,8 +33,10 @@ term(Tree, Lexemes) :- factor(Tree, Lexemes) :-
 	termOperator(Tree, Lexemes),
 	term(Tree, Lexemes).
 
-termOperator(Tree, [L | Lexemes]) :- L '*'.
-termOperator(Tree, [L | Lexemes]) :- L '/'.
+termOperator(Tree, [L | Lexemes]) :- 
+	atom_codes(L, [C | Codes]), C == 42. % 42 = '*' 
+termOperator(Tree, [L | Lexemes]) :-
+	atom_codes(L, [C | Codes]), C == 47. % 47 = '/' 
 
 factor(Tree, Lexemes) :- id(Tree, Lexemes).
 factor(Tree, Lexemes) :- 
@@ -38,8 +44,10 @@ factor(Tree, Lexemes) :-
 	expr(Tree, Lexemes), 
 	rightParen(Lexemes).
 
-leftParen(Tree, [L | Lexemes]) :- L '('.
-rightParen(Tree, [L | Lexemes]) :- L ')'.
+leftParen(Tree, [L | Lexemes]) :- 
+	atom_codes(L, [C | Codes]), C == 40. % 40 = '('
+rightParen(Tree, [L | Lexemes]) :- 
+	atom_codes(L, [C | Codes]), C == 41. % 41 = ')'
 
 id(Tree, [L | Lexemes]) :- L 'a-z'.
 int(Tree, [L | Lexemes]) :- L '0-9'.
