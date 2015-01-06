f823a47c0d58f7d9833bce9891841e152b58823e
Jaernbrand <henrikjarnbrand@gmail.com>
2015-01-06 17:56:03 +0100

    Skrivit lite på evaluate

--- Assignment3-Code/evaluator.pl
+++ Assignment3-Code/evaluator.pl
@@ -1,2 +1,19 @@
 
-evaluate(ParseTree, VariablesIn, VariablesOut).
+evaluate(ParseTree, VariablesIn, Evaluation):-
+	traverseTree(ParseTree, [], ToCalculate),
+	calculate(ToCalculate, Evaluation).
+
+traverseTree(Term, CalcIn, [Term | CalcIn]) :-
+	functor(ParseTree, Name, 0).
+
+traverseTree(Term, CalcIn, CalcOut) :-
+	functor(Term, Name, Arity),
+	visitChildren(Term, Arity, CalcIn, CalcOut).
+	
+visitChildren(Term, I, I, CalcIn, CalcOut):-
+	traverseTree().
+
+visitChildren(Term, ArgIndex, Arity, CalcIn, CalcOut) :-
+	arg(ArgIndex, Term, ArgOut),
+	traverseTree(ArgOut, CalcIn, CalcList),
+	visitChildren(Term, , Arity, CalcList, CalcOut).
